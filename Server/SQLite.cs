815bb9e4f0b2768c285b567a1d5e9e32c945b55a
Trasselwander <adam.hasselwander@gmail.com>
2016-04-13 14:00:52 +0200

    Added SQLite support using mono lib

---
+++ Server/SQLite.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class SQLite
+    {
+        static SqliteConnection GetConnection // 
+        {
+            get
+            {
+                return new SqliteConnection(/* Connection string here */);
+            }
+        }
+    }
+}
