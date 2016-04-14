8c84a1e2dbacced44d87931621d83efe4903c5ba
Trasselwander <adam.hasselwander@gmail.com>
2016-04-14 09:58:16 +0200

    First easeljs commit

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,1 +1,15 @@
 ﻿
+function init() {
+
+    var stage = new createjs.Stage("main");
+
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+
+    stage.addChild(circle);
+    stage.update();
+}
+
+window.onload = init;
