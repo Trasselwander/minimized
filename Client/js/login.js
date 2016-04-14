e4319941e6d7865af8eab8a3971b9851ae46c679
Trasselwander <adam.hasselwander@gmail.com>
2016-04-15 00:01:13 +0200

    Missing network requests

---
+++ Client/js/login.js
@@ -0,0 +1,13 @@
+﻿
+function init() {
+    document.getElementById("reg").onclick = function () {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("register").style.display = null;
+    }
+    document.getElementById("log").onclick = function () {
+        document.getElementById("register").style.display = "none";
+        document.getElementById("login").style.display = null;
+    }
+}
+
+window.onload = init;
