997d3aabbe35abffe49f05c688ac4d81e52562c5
Trasselwander <adam.hasselwander@gmail.com>
2016-04-15 00:02:51 +0200

    Press any key to exit

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,6 +11,8 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
         }
     }
 }
