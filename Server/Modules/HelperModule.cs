e4ad1aa17e91624f73758dc6da2be69fbeeb680f
Trasselwander <adam.hasselwander@gmail.com>
2016-04-15 10:29:10 +0200

    Initial Nancy commit

---
+++ Server/Modules/HelperModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Modules
+{
+    public class HelperModule : NancyModule
+    {
+        public HelperModule(string path) : base(path) { }
+        public HelperModule() { }
+
+
+        public static Response CreateResponse(HttpStatusCode status)
+        {
+            return new Response() { StatusCode = status };
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Mono.Data.Sqlite;
+using Nancy.Hosting.Self;
+using Nancy;
+using Nancy.TinyIoc;
+using Nancy.Bootstrapper;
+using Server.Modules;
+using Nancy.Conventions;
 
 namespace Server
 {
@@ -11,8 +17,51 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
+            using (var host = new NancyHost(new Uri("http://localhost:1234"), new CustomBootstraper()))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class LoginModule : HelperModule
+    {
+        public LoginModule()
+            : base("api/login")
+        {
+            Get["/"] = parameters =>
+            {
+                return true;
+            };
+        }
+    }
+    public class IndexModule : HelperModule
+    {
+        public IndexModule() : base("")
+        {
+            Get[@"/"] = parameters =>
+            {
+                return Response.AsFile(@"../Client/index.html", "text/html");
+            };
+        }
+    }
+
+    public class CustomBootstraper : DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.OnError.AddItemToEndOfPipeline((ctx, exception) => { return null; });
+        }
+
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/", @"../Client/index.html"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/index", @"../Client/index.html"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/login", @"../Client/login.html"));
         }
     }
 }
