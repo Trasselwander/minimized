2284dfc8406b85f1d5cd31da18fa7250713e3eb4
BumpNumb <fennhagen.daniel@hotmail.com>
2016-04-20 14:00:56 +0200

    Get Users By Rank

--- Server/Services/UserService.cs
+++ Server/Services/UserService.cs
@@ -11,6 +11,13 @@ namespace Server.Services
     {
         public User GetUser(string name)
             => SQLite.GetConnection().Query<User>("SELECT * FROM users WHERE name=@name",new { name = name }).FirstOrDefault();
+   
+        public User GetUserFromID(string ID)
+            => SQLite.GetConnection().Query<User>("SELECT * FROM users WHERE ID=@ID", new { ID = ID }).FirstOrDefault();
+   
+        public User GetIDFromRank(string ID)
+            => SQLite.GetConnection().Query<User>("SELECT * FROM users WHERE ID=@ID", new { ID = ID }).FirstOrDefault();
+
 
         public User GetUserByEmail(string email)
             => SQLite.GetConnection().Query<User>("SELECT * FROM users WHERE email=@email", new { email = email }).FirstOrDefault();
@@ -19,18 +26,33 @@ namespace Server.Services
         public UserData GetUserData(int uid)
             => SQLite.GetConnection().Query<UserData>("SELECT * FROM usersdata WHERE UID=@uid", new { uid = uid }).FirstOrDefault();
 
+
         public UserStats GetUserStats(int uid)
-           => SQLite.GetConnection().Query<UserStats>("SELECT * FROM usersstats WHERE UID=@uid", new { uid = uid }).FirstOrDefault();
+            => SQLite.GetConnection().Query<UserStats>("SELECT * FROM usersstats WHERE UID=@uid", new { uid = uid }).FirstOrDefault();
 
 
         public UserData GetUserData(string name)
             => SQLite.GetConnection().Query<UserData>("SELECT * FROM usersdata INNER JOIN users ON userdata.UID=users.ID AND users.name=@name", new { name = name }).FirstOrDefault();
 
         public UserStats GetUserStats(string name)
-           => SQLite.GetConnection().Query<UserStats>("SELECT * FROM userstats INNER JOIN users ON userstats.UID=users.ID AND users.name=@name", new { name = name }).FirstOrDefault();
+            => SQLite.GetConnection().Query<UserStats>("SELECT * FROM userstats INNER JOIN users ON userstats.UID=users.ID AND users.name=@name", new { name = name }).FirstOrDefault();
 
+        public List<User> GetUsersFromRank(int rank)
+        {
+            List<User> uses = null;
+            SQLite.GetConnection().Query<User, UserStats, DBNull>("SELECT * FROM users INNER JOIN userstats ON userstats.UID=users.ID AND abs(@rank - user.rank) <= 3", (users, userstats) =>
+               {
+                   users.userStats = userstats;
+                   uses.Add(users);
+
+                   return null;
+               }, new { rank = rank }).FirstOrDefault();
+            return uses;
+        }
 
         public class User {
+            public UserData userData { get; set; }
+            public UserStats userStats { get; set; }
             public int ID { get; set; }
             public string name { get; set; }
             public string hash { get; set; }
