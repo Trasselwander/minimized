66f846149bdf74cade4958a4009e918dc0c90500
BumpNumb <fennhagen.daniel@hotmail.com>
2016-04-21 13:06:12 +0200

    Added Enter support for login & register

--- Client/js/login.js
+++ Client/js/login.js
@@ -25,8 +25,34 @@ window.addEventListener("load", () => {
             }
         });
 
+
+
         //SendRequest();
     }
+
+    document.getElementById("login_password").onkeypress = (e) => { //login from enter
+
+        if (e.keyCode == 13) {
+            user.name = document.getElementById("login_name").value;
+            user.password = superhash(document.getElementById("login_password").value);
+
+            //function sendRequest(action, callback, sign) { // implement send data.
+
+            console.log(user.name, user.password);
+            sendRequest("/login/", (text, error) => {
+                if (!error) {
+                    toggleScreen(screens.overview.elm);
+                    console.log(error + "hej");
+                    user.save();
+                }
+                else {
+                    toggleScreen(screens.login.elm);
+
+                }
+            });
+        }
+    }
+
     document.getElementById("register_btn").onclick = () => {
         user.name = document.getElementById("register_name").value;
         user.password = superhash(document.getElementById("register_password").value);
@@ -49,6 +75,34 @@ window.addEventListener("load", () => {
             }
         });
     }
+
+    document.getElementById("register_email").onkeypress = (e) => { // resgister from enter
+
+        if (e.keyCode == 13) {
+            user.name = document.getElementById("register_name").value;
+            user.password = superhash(document.getElementById("register_password").value);
+            user.email = document.getElementById("register_email").value;
+
+            if (document.getElementById("register_password_repeat").classList.contains("wrong")) {
+                swal({
+                    title: "Error!",
+                    text: "Passwords do not match!",
+                    type: "error",
+                });
+                return;
+            }
+
+            console.log(user.name, user.password);
+            sendRequest("/register/" + (user.email ? btoa(user.email) : ""), (text, error) => {
+                if (!error) {
+                    toggleScreen(screens.overview.elm);
+                    user.save();
+                }
+            });
+
+        }
+    }
+
 
     document.getElementById("register_password_repeat").onkeyup = () => {
         if (document.getElementById("register_password").value != document.getElementById("register_password_repeat").value) {
