3407468aaa51253fb24d8e15d985631b1265b28d
Trasselwander <adam.hasselwander@gmail.com>
2016-04-24 19:53:55 +0200

    Minor bugs

--- Client/js/attack.js
+++ Client/js/attack.js
@@ -3,11 +3,12 @@ document.getElementById("attack").addEventListener("toggled", () => {
     var hex = new hexstat("canvas_attack");
     setTimeout(function () {
         hex.maxskill = 40; // max of animate arguments
-        hex.animate(40, 10, 30, 15, 8, 35, 10, 30, 15, 8, 35, 40, "rgba(38, 94, 209, 0.5)", "rgba(18, 149, 33, 0.5)")
+        hex.animate([{ "life": 5, "speed": 10, "physicalattack": 20, "physicaldefence": 20, "magicattack": 10, "magicdefence": 15, "color": "rgba(38, 94, 209, 0.5)" },
+            { "life": 5, "speed": 30, "physicalattack": 20, "physicaldefence": 10, "magicattack": 10, "magicdefence": 30, "color": "rgba(18, 149, 33, 0.5)" }])
     }, 1000);
     console.log("updated");
 
-    getPlayers();
+    //getPlayers();
 });
 
 
