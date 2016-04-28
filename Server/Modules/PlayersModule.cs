e37389918a5995075cf188ccca2fb8cb80f760f3
Trasselwander <adam.hasselwander@gmail.com>
2016-04-28 11:56:49 +0200

    Fixed register after db update

--- Server/Modules/PlayersModule.cs
+++ Server/Modules/PlayersModule.cs
@@ -24,7 +24,7 @@ namespace Server.Modules
             Get["/player"] = parameters =>
             {
                 Services.UserService.User user = AuthorizeUser();
-                Users.GetUserDataAndStats(user);
+                Users.GetUserStats(user);
 
                 return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(user));
             };
--- Server/Services/UserService.cs
+++ Server/Services/UserService.cs
@@ -16,73 +16,60 @@ namespace Server.Services
         public User GetUserByEmail(string email)
             => SQLite.GetConnection().Query<User>("SELECT * FROM users WHERE email=@email", new { email = email }).FirstOrDefault();
 
-
-        public UserData GetUserData(int uid)
-            => SQLite.GetConnection().Query<UserData>("SELECT * FROM userdata WHERE UID=@uid", new { uid = uid }).FirstOrDefault();
-
-        public UserData GetUserData(string name)
-            => SQLite.GetConnection().Query<UserData>("SELECT * FROM usersdata INNER JOIN users ON userdata.UID=users.ID AND users.name=@name", new { name = name }).FirstOrDefault();
-
-
         public UserStats GetUserStats(int uid)
             => SQLite.GetConnection().Query<UserStats>("SELECT * FROM userstats WHERE UID=@uid", new { uid = uid }).FirstOrDefault();
 
         public UserStats GetUserStats(string name)
             => SQLite.GetConnection().Query<UserStats>("SELECT * FROM userstats INNER JOIN users ON userstats.UID=users.ID AND users.name=@name", new { name = name }).FirstOrDefault();
 
-        public void GetUserDataAndStats(User user)
-        {
-            user.userData = GetUserData(user.ID);
-            user.userStats = GetUserStats(user.ID);
-        }
+        public void GetUserStats(User user)
+            => user.userStats = GetUserStats(user.ID);
+
+        public void CreateLeague(Leauge l)
+            => SQLite.GetConnection().Query("INSERT INTO leagues (name, start, end) VALUES (@name, @start, @end)", new { l.name, l.start, l.end });
+
 
         public User CreateUser(string name, string hash, string email)
         {
             User u = new User() { name = name, hash = hash, email = email };
             CryptoService.HashAndSavePassword(hash, u);
-            SQLite.GetConnection().QueryMultiple(@"INSERT INTO users (name, email, hash, salt, lastloggedin)VALUES(@name, @email, @hash, @salt, @time); 
+            SQLite.GetConnection().QueryMultiple(@"INSERT INTO users (name, email, hash, salt, lastloggedin, bestrank, age) SELECT @name as name, @email as email, @hash as hash, @salt as salt, @time as lastloggedin, (SELECT COUNT(*) FROM users) AS bestrank, @time AS age; 
                                                    INSERT INTO userstats (UID, bestrank)
-                                                   SELECT ID AS UID, 
-	                                                   (SELECT COUNT(*) FROM users) AS bestrank 
-                                                   FROM users WHERE users.name = @name;
-                                                   INSERT INTO userdata (UID, bestrank, age) 
-                                                   SELECT ID AS UID, 
-	                                                   (SELECT COUNT(*) FROM users) AS bestrank, 
-	                                                   @time as time 
-                                                   FROM users WHERE users.name = @name;", new { name = u.name, email = u.email, hash = u.hash, salt = u.salt, time = (long)((DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds) });
+	                                                   SELECT (SELECT ID FROM users WHERE users.name = @name) AS UID, COUNT(*) AS bestrank FROM users;"
+                                                   , new { name = u.name, email = u.email, hash = u.hash, salt = u.salt, time = (long)((DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds) });
 
             return u;
         }
 
         public int GetUserRank2(User u)
-            => SQLite.GetConnection().Query<int>("SELECT COUNT(*) FROM userdata INNER JOIN userstats ON userstats.score >  WHERE userdata.LID = @lid ", new { lid = u.userData.LID }).FirstOrDefault();
+            => SQLite.GetConnection().Query<int>("SELECT COUNT(*) FROM users INNER JOIN userstats ON userstats.score >  WHERE userdata.LID = @lid ", new { lid = u.LID }).FirstOrDefault();
 
         public int GetUserRank(User u)
-            => SQLite.GetConnection().Query<int>(@"SELECT COUNT(*) as s FROM userdata WHERE userdata.LID = @lid
-                                                      SELECT * FROM s WHERE s.UID = @uid", new { lid = u.userData.LID, uid = u.ID }).FirstOrDefault();
+            => SQLite.GetConnection().Query<int>(@"SELECT COUNT(*) as s FROM users WHERE users.LID = @lid
+                                                      SELECT * FROM s WHERE s.UID = @uid", new { lid = u.LID, uid = u.ID }).FirstOrDefault();
 
-        public User GetOpponent(User u)
-        {
-            if (u.userStats == null)
-                u.userStats = GetUserStats(u.ID);
+        //public User GetOpponent(User u)
+        //{
+        //    if (u.userStats == null)
+        //        u.userStats = GetUserStats(u.ID);
 
-            int worst_rank = SQLite.GetConnection().Query<int>("SELECT COUNT(*) FROM users INNER JOIN userdata ON userdata.LID=@lid", new { lid = u.userData.LID }).FirstOrDefault();
+        //    int worst_rank = SQLite.GetConnection().Query<int>("SELECT COUNT(*) FROM users INNER JOIN userdata ON userdata.LID=@lid", new { lid = u.userData.LID }).FirstOrDefault();
 
-            Random rand = new Random(); // reuse this if you are generating many
-            int random_rank = u.userStats.rank;
+        //    Random rand = new Random(); // reuse this if you are generating many
+        //    int random_rank = u.userStats.rank;
 
-            while (random_rank == u.userStats.rank) // There may be errors inside of this loop.
-            {
-                if (u.userStats.rank >= 5)
-                    random_rank = rand.Next(1, 10);
-                else if (u.userStats.rank + 5 > worst_rank)
-                    random_rank = rand.Next(worst_rank - 10, worst_rank);
-                else
-                    random_rank = rand.Next(u.userStats.rank - 5, u.userStats.rank + 5);
-            }
+        //    while (random_rank == u.userStats.rank) // There may be errors inside of this loop.
+        //    {
+        //        if (u.userStats.rank >= 5)
+        //            random_rank = rand.Next(1, 10);
+        //        else if (u.userStats.rank + 5 > worst_rank)
+        //            random_rank = rand.Next(worst_rank - 10, worst_rank);
+        //        else
+        //            random_rank = rand.Next(u.userStats.rank - 5, u.userStats.rank + 5);
+        //    }
 
-            return SQLite.GetConnection().Query<User>("SELECT * FROM users INNER JOIN userdata ON userdata.LID=@lid ORDER BY userdata.score LIMIT 1 OFFSET @rank", new { lid = u.userData.LID, rank = random_rank }).FirstOrDefault();
-        }
+        //    return SQLite.GetConnection().Query<User>("SELECT * FROM users INNER JOIN userdata ON userdata.LID=@lid ORDER BY userdata.score LIMIT 1 OFFSET @rank", new { lid = u.userData.LID, rank = random_rank }).FirstOrDefault();
+        //}
 
         public void JoinLeauge(User u, int lid)
         {
@@ -91,8 +78,8 @@ namespace Server.Services
             SQLite.GetConnection().QueryMultiple(@"DELETE FROM userstats WHERE userstats.uid = @uid;
                                                    INSERT INTO userstats (UID, bestrank)
                                                    SELECT @uid AS UID,
-	                                                   (SELECT COUNT(*) FROM userdata WHERE userdata.lid = @lid) AS bestrank;
-                                                   UPDATE userdata SET LID=@lid WHERE userdata.UID = @uid;", new { uid = u.ID, lid = lid });
+	                                                   (SELECT COUNT(*) FROM users WHERE users.lid = @lid) AS bestrank;
+                                                   UPDATE users SET LID=@lid WHERE users.ID = @uid;", new { uid = u.ID, lid = lid });
         }
 
         public List<Leauge> GetLeagues()
@@ -101,7 +88,7 @@ namespace Server.Services
 
             foreach (var l in leauge)
             {
-                var d = SQLite.GetConnection().Query<Leauge>(@"SELECT * FROM userstats INNER JOIN userdata ON userstats.UID=userdata.UID AND userdata.LID=@lid", new { lid = l.ID }).FirstOrDefault();
+                var d = SQLite.GetConnection().Query<Leauge>(@"SELECT * FROM userstats INNER JOIN users ON userstats.UID=users.ID AND users.LID=@lid", new { lid = l.ID }).FirstOrDefault();
 
                 var data = SQLite.GetConnection().Query<Leauge>(@"SELECT 
                                                                     SUM(userstats.life), 
@@ -110,7 +97,7 @@ namespace Server.Services
                                                                     SUM(userstats.physicaldefence), 
                                                                     SUM(userstats.magicattack), 
                                                                     SUM(userstats.magicdefence) 
-                                                                 FROM userstats INNER JOIN userdata ON userstats.UID=userdata.UID AND userdata.LID=@lid", new { lid = l.ID }).FirstOrDefault();
+                                                                 FROM userstats INNER JOIN users ON userstats.UID=users.ID AND users.LID=@lid", new { lid = l.ID }).FirstOrDefault();
 
                 l.life = data.life;
                 l.speed = data.speed;
@@ -122,10 +109,6 @@ namespace Server.Services
 
             return leauge;
         }
-
-
-        public void CreateLeague(Leauge l)
-            => SQLite.GetConnection().Query("INSERT INTO leagues (name, start, end) VALUES (@name, @start, @end)", new { l.name, l.start, l.end });
 
         public class Leauge
         {
@@ -152,7 +135,6 @@ namespace Server.Services
 
         public class User
         {
-            public UserData userData { get; set; }
             public UserStats userStats { get; set; }
             [JsonIgnore]
             public long lastloggedin { get; set; }
@@ -164,18 +146,13 @@ namespace Server.Services
             public string salt { get; set; }
             [JsonIgnore]
             public string email { get; set; }
-        }
 
-        public class UserData
-        {
-            public int ID { get; set; }
-            public int UID { get; set; }
             public int? LID { get; set; }
             public int? HID { get; set; }
             public int bestrank { get; set; }
             public long age { get; set; }
-            public int wins { get; set; }
-            public int losses { get; set; }
+            public int totalwins { get; set; }
+            public int totallosses { get; set; }
             public int skillpoints { get; set; }
         }
 
