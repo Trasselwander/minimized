a5194cc73b9645eff65d43c8672013b6c6876ba4
Trasselwander <adam.hasselwander@gmail.com>
2016-05-01 21:43:21 +0200

    Kolla! Kolla!

--- Client/js/leaguetoplist.js
+++ Client/js/leaguetoplist.js
@@ -1,4 +1,6 @@
 ﻿
+var top_hex = {};
+
 screens.leaguetoplist.elm.addEventListener("toplist", () => { // Is this the right event?
     var tbl = screens.leaguetoplist.elm.getElementsByClassName("stats-table")[0];
     var bdy = tbl.getElementsByTagName("tbody")[0];
@@ -43,7 +45,7 @@ screens.leaguetoplist.elm.addEventListener("toplist", () => { // Is this the rig
                 tdn.innerHTML = data.name;
                 tds.innerHTML = data.userStats.score;
                 tdl.innerHTML = data.userStats.level;
-                this.dataset.open = null;
+                this.dataset.open = top_hex[data.name] = null;
                 return;
             }
 
@@ -51,10 +53,22 @@ screens.leaguetoplist.elm.addEventListener("toplist", () => { // Is this the rig
 
 
 
-            var l =  "<br/>";
-            tdr.innerHTML += l + "Bästa rank: " + data.bestrank;
-            tds.innerHTML += l + "Vunnit: " + data.totalwins + l + "Förlorat: " + data.totallosses;
-            tdn.innerHTML += l + "EXP: " + data.userStats.exp;
+            var l = "<br/>";
+            tdr.innerHTML += l + "<span>Bästa rank: " + data.bestrank + "</span>";
+            tds.innerHTML += l + "<span>Vunnit: " + data.totalwins + l + "Förlorat: " + data.totallosses + "</span>";
+            tdn.innerHTML += l + "<span>EXP: " + data.userStats.exp + "</span>";
+
+            var can = document.createElement("canvas");
+            can.id = "mini_" + data.name;
+            can.width = can.height = 70;
+            tdl.appendChild(can);
+
+            top_hex[data.name] = new hexstat(can.id);
+
+            top_hex[data.name].maxskill = Math.max(data.userStats.life, data.userStats.speed, data.userStats.physicalattack, data.userStats.physicaldefence, data.userStats.magicattack, data.userStats.magicdefence);
+            data.userStats.color = "rgba(57, 174, 221, 0.65)";
+            top_hex[data.name].animate([data.userStats]);
+            //console.log(data);
         }
 
         bdy.appendChild(tr);
