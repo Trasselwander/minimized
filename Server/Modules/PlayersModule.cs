9a8276c877f85ed6f23974de3be8c74d77d3a166
Trasselwander <adam.hasselwander@gmail.com>
2016-05-01 15:43:20 +0200

    Fixed enemy broke levelup

--- Server/Modules/PlayersModule.cs
+++ Server/Modules/PlayersModule.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Nancy;
 using Newtonsoft.Json;
+using static Server.Services.UserService;
 
 namespace Server.Modules
 {
@@ -15,19 +16,19 @@ namespace Server.Modules
         {
             Get["/top/{id}"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(Users.GetTopByLeauge(parameters.id)));
             };
 
             Get["/top10/{id}"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(Users.GetTop10ByLeauge(parameters.id)));
             };
 
             Get["/player"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 Users.GetUserStats(user);
                 Users.GetUserLeague(user);
 
@@ -36,15 +37,19 @@ namespace Server.Modules
 
             Get["/enemy"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 Users.GetUserStats(user);
 
-                return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(Users.GetCloseUsersByScore(user)));
+                User[] users = Users.GetCloseUsersByScore(user).ToArray();
+                int random = new Random().Next(0, users.Length - 1);
+
+                Users.GetUserStats(users[random]);
+                return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(users[random]));
             };
 
             Get["/attack/{id}"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 Users.GetUserStats(user);
 
                 return CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(Users.GetCloseUsersByScore(user)));
@@ -52,42 +57,13 @@ namespace Server.Modules
 
             Get["/levelup/{id}"] = parameters =>
             {
-                Services.UserService.User user = AuthorizeUser();
+                User user = AuthorizeUser();
                 Users.GetUserStats(user);
 
                 if (user.userStats == null || user.userStats.skillpoints < 1)
                     return CreateResponse(HttpStatusCode.BadRequest);
-                switch (((string)parameters.id).ToLower())
-                {
-                    case "1":
-                    case "life":
-                        Users.IncrementStat("life", user);
-                        break;
-                    case "2":
-                    case "speed":
-                        Users.IncrementStat("speed", user);
-                        break;
-                    case "3":
-                    case "physicalattack":
-                        Users.IncrementStat("physicalattack", user);
-                        break;
-                    case "4":
-                    case "physicaldefence":
-                        Users.IncrementStat("physicaldefence", user);
-                        break;
-                    case "5":
-                    case "magicattack":
-                        Users.IncrementStat("magicattack", user);
-                        break;
-                    case "6":
-                    case "magicdefence":
-                        Users.IncrementStat("magicdefence", user);
-                        break;
 
-                    default:
-                        return CreateResponse(HttpStatusCode.BadRequest);
-                }
-
+                Users.IncrementStat(((string)parameters.id).ToLower(), user);
                 return CreateResponse(HttpStatusCode.OK);
             };
         }
