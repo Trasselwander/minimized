2b326874da95909df6b2dfeb94465c096e2331d5
Trasselwander <adam.hasselwander@gmail.com>
2016-08-17 22:10:56 +0200

    Some top10 fixes in overview

--- Client/js/players.js
+++ Client/js/players.js
@@ -1,32 +1,33 @@
 ﻿
 function getPlayer() {
-    sendRequest("/players/player", (test, error) => {
-        if (!error) {
-            player = JSON.parse(test);
-            screens.overview.elm.dispatchEvent(new Event("player"));
-        }
-    });
+     sendRequest("/players/player", (test, error) => {
+          if (!error) {
+               player = JSON.parse(test);
+               screens.overview.elm.dispatchEvent(new Event("player"));
+          }
+     });
 }
 
 function getEnemy() {
-    sendRequest("/players/enemy", (test, error) => {
-        if (!error) {
-            enemy = JSON.parse(test);
-            screens.attack.elm.dispatchEvent(new Event("enemy"));
-        }
-    });
+     sendRequest("/players/enemy", (test, error) => {
+          if (!error) {
+               enemy = JSON.parse(test);
+               screens.attack.elm.dispatchEvent(new Event("enemy"));
+          }
+     });
 }
 
 function getTop10() {
-    if (!player.LID) {
-        screens.overview.elm.dispatchEvent(new Event("top10"));
-        return;
-    }
+     if (!player.LID) {
+          top10 = null;
+          screens.overview.elm.dispatchEvent(new Event("top10"));
+          return;
+     }
 
-    sendRequest("/leagues/top10/" + player.LID, (test, error) => {
-        if (!error) {
-            top10 = JSON.parse(test);
-            screens.overview.elm.dispatchEvent(new Event("top10"));
-        }
-    });
+     sendRequest("/leagues/top10/" + player.LID, (test, error) => {
+          if (!error) {
+               top10 = JSON.parse(test);
+               screens.overview.elm.dispatchEvent(new Event("top10"));
+          }
+     });
 }
