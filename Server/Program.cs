3406790359bfa51ac82e57e8798623848818ff26
Trasselwander <adam.hasselwander@gmail.com>
2016-12-04 23:19:49 +0100

    Mono support
    less cpu

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Nancy.Hosting.Self;
 using Server.Services;
-using System.Threading.Tasks;
+using Mono.Unix.Native;
+using Mono.Unix;
 
 namespace Server
 {
@@ -14,55 +15,58 @@ namespace Server
             Console.WriteLine("Creating database...");
             SQLite.InitDatabase();
 
-            if (args.Length != 0)
+            if (args.Length != 0) ProcessCommands(args);
+
+            Console.WriteLine("Running in server mode.");
+
+            using (var host = new NancyHost(new Uri("http://localhost:1235"), new CustomBootstraper()))
             {
-                Console.WriteLine("Running in client mode.");
+                host.Start();
+                Console.WriteLine("Running nancy on http://localhost:1235");
 
-                CommandService.RegisteredCommands.Add("league", new Commands.LeagueCommand());
-                CommandService.RegisteredCommands.Add("help", new Commands.HelpCommand());
-                CommandService.RegisteredCommands.Add("exit", new Commands.ExitCommand());
-                CommandService.RegisteredCommands.Add("db", new Commands.DbCommand());
-                CommandService.RegisteredCommands.Add("cli", new Commands.CliCommand());
+                Console.WriteLine("Enabling timers..");
+                TimerService.InitTimers();
 
-                Console.WriteLine("Write exit to terminate and help for some help");
+                Console.WriteLine("Done.");
 
-                string cmd = string.Join(" ", args);
-                while (true)
-                {
-                    try
-                    {
-                        CommandService.Process(cmd);
-                        if (stop) return;
-                    }
-                    catch (Exception)
-                    {
-                        Console.Write("A command should not be able to crash the whole server, something smells rotten... *YOUR CODE*");
-                    }
-                    Console.WriteLine();
-
-                    cmd = Console.ReadLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Running in server mode.");
-
-                using (var host = new NancyHost(new Uri("http://localhost:1235"), new CustomBootstraper()))
-                {
-                    host.Start();
-                    Console.WriteLine("Running nancy on http://localhost:1235");
-
-                    Console.WriteLine("Enabling timers..");
-                    TimerService.InitTimers();
-
-                    Console.WriteLine("Done.");
-                    while (true) AsyncSleep(10).Wait();
-                }
+                if (Type.GetType("Mono.Runtime") != null)
+                    UnixSignal.WaitAny(new UnixSignal[] {
+                        new UnixSignal (Signum.SIGINT),
+                        new UnixSignal (Signum.SIGTERM),
+                    }, -1);
+                else
+                    Console.ReadLine();
             }
         }
-        static async Task AsyncSleep(int milliseconds)
+
+        static void ProcessCommands(string[] args)
         {
-           await Task.Delay(milliseconds);
+            Console.WriteLine("Running in client mode.");
+
+            CommandService.RegisteredCommands.Add("league", new Commands.LeagueCommand());
+            CommandService.RegisteredCommands.Add("help", new Commands.HelpCommand());
+            CommandService.RegisteredCommands.Add("exit", new Commands.ExitCommand());
+            CommandService.RegisteredCommands.Add("db", new Commands.DbCommand());
+            CommandService.RegisteredCommands.Add("cli", new Commands.CliCommand());
+
+            Console.WriteLine("Write exit to terminate and help for some help");
+
+            string cmd = string.Join(" ", args);
+            while (true)
+            {
+                try
+                {
+                    CommandService.Process(cmd);
+                    if (stop) return;
+                }
+                catch (Exception)
+                {
+                    Console.Write("A command should not be able to crash the whole server, something smells rotten... *YOUR CODE*");
+                }
+                Console.WriteLine();
+
+                cmd = Console.ReadLine();
+            }
         }
     }
 }
